75ff0cec21409569e4be3d88e100f78c1f11d81b
kozerka <kozerka@gmail.com>
2024-04-06 19:35:30 +0200

    Ad a from to create new posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,8 @@ class BlogPostsController < ApplicationController
   def show
     @blog_post = BlogPost.find(params[:id])
   end
+
+  def new
+    @blog_post = BlogPost.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'trzecia', to: 'pages#trzecia'
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   # Trasa główna
   root 'home#index'
 
